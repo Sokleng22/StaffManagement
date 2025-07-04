c49e960fd0898220776a452695831c75a6f00b9f
Sokleng22 <lenglengorisakak@gmail.com>
2025-07-04 10:25:06 +0700

    fix App test

--- StaffManagement.Web/src/App.test.js
+++ StaffManagement.Web/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders staff management system', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleElement = screen.getByText(/Staff Management System/i);
+  expect(titleElement).toBeInTheDocument();
 });
